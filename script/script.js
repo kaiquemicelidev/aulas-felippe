de5d6571494e2aab4c7df085aa8421ec51236e40
KaiqueMiceliDev <kaiquemiceli.dev@gmail.com>
2024-03-20 22:55:10 -0300

    Inserted Menu Popdraw

--- script/script.js
+++ script/script.js
@@ -1,20 +1,34 @@
+
+//AQUI ESTA SENDO CONFIGURADO AO INICIALIZAR A PAGINA
 $(document).ready(function(){
-    $(".section_home").load('home.html');
-    $(".section_about").load('about.html');
-    $(".section_projects").load('projects.html');
-    $("#section_contact").load('contact.html');
+    $(".section_home").load('home.html'); //CARREGA A PAGINA HOME NO MAIN
+    $(".section_about").load('about.html'); //CARREGA A PAGINA ABOUT 
+    $(".section_projects").load('projects.html'); // CARREGA A PAGINA PROJECTS
+    $("#section_contact").load('contact.html'); // CARREGA A PAGINA CONTACT
 });
 
-$("#menuItem_home").on('click', function(){
-    
-});
-
+//AO CLICAR NO BOTAO MENU, ELE VAI ABRIR O MENU QUE ESTAVA DISPLAY NONE E TBM A INIVISIBLE BOX
 $(".menuButton").on('click', function(){
     $('.invisibleBox').css('display','block');
     $('.menuRight').css('display','block');
 });
 
+//AO CLICAR NO INVISIBLE BOX ELE VAI FECHAR O MENU E TBM O BOX e mPOPDRAW (AMBOS SE TIVEREM ABERTOS)
 $('.invisibleBox').on('click', function(){
-    $('.menuRight').css('display','none');
+    if($('.menuRight').css('display') == 'block'){
+        $('.menuRight').css('display','none');
+    }else if($('.caixaVerde').css('display') == 'block'){
+        $('.caixaVerde').css('display','none');
+    }else{
+        $('.invisibleBox').css('display','none');
+    }
+
     $('.invisibleBox').css('display','none');
+});
+
+
+//AO CLICAR NO MENU ITEM HOME, ABRE MENU POPDRAW
+$('#menuItem_home').on('click', function(){
+    $('.invisibleBox').css('display','block');
+    $('.caixaVerde').css('display','block');
 });
