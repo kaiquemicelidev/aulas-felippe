6d0f0d3a22972aef157a68c26bdcb4fbeee26f9f
Kaique Miceli <163534270+kaiquemicelidev@users.noreply.github.com>
2024-03-23 19:33:01 -0300

    add index2

---
+++ script/script2.js
@@ -0,0 +1,19 @@
+$("#btn").on('click', function(){
+
+    var texto = 'Este é meu texto';
+    $("#conteudo").append("<p>Este é um texto adicionado"); //append = adiciona
+    $("#conteudo").html("<p>Este é um texto adicionado"); //html = reescreve
+    // remove() remove o elemento
+    //load = carrega conteudo externo para dentro do elemento
+    // addClass= adiciona classe //removeClass = remove classe
+    // css = escreve novo css
+
+    //.on (o que acontece?)
+
+    //document (é a página em si)
+    //click = ação de clicar
+    //ação ready =ação que indica carregamento
+    // function(){} //função anonima
+
+
+});
